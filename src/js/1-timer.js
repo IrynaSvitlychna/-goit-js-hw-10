7b5190094f7f88ba685d826268e7366105c570bd
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-01-20 02:39:28 +0200

    add HM10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,6 +3,63 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+import xOctagon from "../img/bi_x-octagon.svg";
+
+let elements = document.getElementsByClassName('label');
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].textContent = elements[i].textContent.toUpperCase();
+}
+
+
+const startBtn = document.querySelector('[data-start]');
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  locale: {
+    firstDayOfWeek: 1,
+  },
+  onOpen: function daysNames() {
+    document.querySelectorAll('.flatpickr-weekday').forEach(el => {
+      el.textContent = el.textContent.trim().slice(0, 2);
+    });
+  },
+  onClose: function (selectedDates) {
+    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0].getTime();
+    if (userSelectedDate >= Date.now()) {
+      startBtn.removeAttribute('disabled');
+      startBtn.classList.add('button-enabled');
+    } else {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        iconUrl: `${xOctagon}`,
+        backgroundColor: '#EF4040',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        messageSize: '16px',
+        progressBarColor: '#B51B1B',
+      });
+      startBtn.setAttribute('disabled', true);
+      startBtn.classList.remove('button-enabled');
+    }
+  },
+};
+
+const dateTimeInput = flatpickr('#datetime-picker', options);
+
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -11,18 +68,43 @@ function convertMs(ms) {
     const hour = minute * 60;
     const day = hour * 24;
   
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+     // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
     return { days, hours, minutes, seconds };
   }
   
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+   startBtn.addEventListener('click', () => {
+    startBtn.setAttribute('disabled', true);
+    startBtn.classList.remove('button-enabled');
+       
+    setInterval (() => {
+      const currentTime = Date.now();
+      const difference = userSelectedDate - currentTime;
+      const time = convertMs(difference);
+      const { days, hours, minutes, seconds } = time;
+
+    if (days >= 0 || hours >= 0 || minutes >= 0 || seconds >= 0) {
+      document.querySelector('[data-days').textContent = days
+        .toString()
+        .padStart(2, '0');
+      document.querySelector('[data-hours]').textContent = hours
+        .toString()
+        .padStart(2, '0');
+      document.querySelector('[data-minutes]').textContent = minutes
+        .toString()
+        .padStart(2, '0');
+      document.querySelector('[data-seconds]').textContent = seconds
+        .toString()
+        .padStart(2, '0');
+    }
+  }, 1000);
+});
+      
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,56 @@
 //  Описаний у документації
-import iziToast from "izitoast";
+import izitoast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+import vector from "../img/Vector.svg";
+import xOctagon from "../img/bi_x-octagon.svg";
+
+createForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const delay = event.currentTarget.elements.delay.value;
+    const radioChecked = document.querySelector('input[name="state"]:checked');
+  
+     
+    const promise = new Promise((resolve, reject) => {
+      if (radioChecked.value === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    });
+  
+    
+    promise
+      .then(delay => {
+        setTimeout(() => {
+          iziToast.success({
+            title: 'OK',
+            message: `Fulfilled promise in ${delay}ms`,
+            position: 'topRight',
+            iconUrl: `${vector}`,
+            backgroundColor: '#59A10D',
+            titleColor: '#fff',
+            messageColor: '#fff',
+            messageSize: '16px',
+            progressBarColor: '#B5EA7C',
+          });
+        }, delay);
+      })
+      .catch(delay => {
+        setTimeout(() => {
+          iziToast.error({
+            title: 'Error',
+            message: `Rejected promise in ${delay}ms`,
+            position: 'topRight',
+            iconUrl: `${xOctagon}`,
+            backgroundColor: '#EF4040',
+            titleColor: '#fff',
+            messageColor: '#fff',
+            messageSize: '16px',
+            progressBarColor: '#FFBEBE',
+          });
+        }, delay);
+      });
+    createForm.reset();
+  });
